0acfb8caa374d32fc97d482371c5e4a3ba567ff1
Tomás Ó Dálaigh <tom1996.18@gmail.com>
2023-06-28 17:27:15 +0100

    Initial commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Patch Creation') {
+            steps {
+                script {
+                    // Download the zip file
+                    sh 'curl -o /path/to/download/directory/myZipFile.zip <URL>'
+                    // Unzip the file
+                    sh 'unzip /path/to/download/directory/myZipFile.zip -d /path/to/unzipped/directory'
+                    // Patch creation
+                    sh """
+                    java -jar /path/to/patch-creator.jar -p sso -v 7.6.2 -t -debug -i 12345 -d "test patch" -c /path/to/unzipped/directory/keycloak-services-18.0.4-patch.jar
+                    """
+                }
+            }
+        }
+        stage('Patch Implementation') {
+            steps {
+                sh """
+                service myJavaApplication stop
+                mv /path/to/myJavaApplication.jar /path/to/myJavaApplication.jar.backup
+                cp /path/to/generated/patch/file /path/to/myJavaApplication.jar
+                service myJavaApplication start
+                """
+            }
+        }
+        stage('Override If Necessary') {
+            when {
+                // Here, condition should be an expression that evaluates to true if an override is necessary
+                expression { return <CONDITION> }
+            }
+            steps {
+                // If an override is necessary, include steps for how to handle it here
+                // For example:
+                sh """
+                cp /path/to/override/file /path/to/myJavaApplication.jar
+                """
+            }
+        }
+    }
+    post {
+        success {
+            umbSend 'rh-sso.server.end.success' // Send a UMB message when the pipeline completes successfully
+        }
+        failure {
+            umbSend 'rh-sso.server.end.error.failure' // Send a UMB message when the pipeline fails
+        }
+        aborted {
+            umbSend 'rh-sso.server.end.error.aborted' // Send a UMB message when the pipeline is aborted
+        }
+    }
+}
